242c1854125ee1cf77504d6ac7973398bc5b0e20
Brent Eerlingen <>
2018-01-24 10:52:36 +0100

    Singleton Pattern

---
+++ SingletonPatternDemo/SingletonPatternDemo/Potus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SingletonPatternDemo
+{
+    public class Potus
+    {
+        private static Potus potus = new Potus();
+
+        private Potus()
+        {
+        }
+
+        public static Potus GetPotus()
+        {
+            return potus;
+        }
+
+        public void RandomTweet(){
+            Console.WriteLine("This is fake news!");
+        }
+    }
+}
---
+++ SingletonPatternDemo/SingletonPatternDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will give you a Compile Time Error: the constructor is not visible in this class
+            // var potus = new Potus();
+
+            var potus = Potus.GetPotus();
+
+            potus.RandomTweet();
+        }
+    }
+}
